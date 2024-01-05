c2e9846f9018c93e371566708fbafa511548a28e
空巷一人 <1530688385@qq.com>
2024-01-05 19:54:20 +0800

    1

---
+++ README.md
@@ -0,0 +1,9 @@
+# 说明
+
+原项目:https://github.com/zhheo/Post-Abstract-AI
+
+## 使用 ts+vite+sass 重新编译
+
+<link rel="stylesheet" href="https://cdn1.tianli0.top/gh/kongxiangyiren/tianli_gpt/tianli_gpt.css">
+
+<script src="https://cdn1.tianli0.top/gh/kongxiangyiren/tianli_gpt/tianli_gpt.min.js"></script>
---
+++ scripts/build.ts
@@ -0,0 +1,57 @@
+import { InlineConfig, build } from "vite";
+import { resolve, dirname, parse } from "path";
+import { fileURLToPath } from "url";
+import dts from "vite-plugin-dts";
+import * as sass from "sass";
+import { writeFileSync } from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const entries = ["../src/tianli_gpt.ts"];
+
+// 动态生成配置build配置选项
+const profiles = entries.flatMap((file): InlineConfig => {
+  return {
+    build: {
+      emptyOutDir: false,
+      // 禁用压缩报告
+      reportCompressedSize: false,
+      lib: {
+        name: "tianliGPT",
+        formats: ["umd"],
+        entry: resolve(__dirname, file),
+        fileName: (format) => parse(file).name + ".min.js",
+      },
+    },
+    plugins: [
+      dts({
+        entryRoot: "src",
+        copyDtsFiles: false,
+        insertTypesEntry: true,
+        // 不需要.d.ts 文件
+        beforeWriteFile: (filePath: string, content: string) => false,
+        tsconfigPath: resolve(__dirname, "../tsconfig.app.json"),
+      }),
+    ],
+  };
+});
+
+// 入口
+async function main() {
+  // 遍历配置选项，手动调用build方法
+  for (const profile of profiles) {
+    await build(profile);
+  }
+}
+
+await main();
+
+// 编译sass
+const result = await sass.compileAsync(
+  resolve(__dirname, "../src/assets/index.scss"),
+  {
+    style: "compressed",
+  }
+);
+writeFileSync(resolve(__dirname, "../dist/tianli_gpt.css"), result.css);
